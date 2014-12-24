8094c430eb8add936856aa9297155a80f0c924e3
Andrei Sumar <andrey.sumar@gmail.com>
2014-12-24 10:15:02 +0300

    Cleaned auto-generated configs, updated .gitignore.

--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -9,33 +9,26 @@ var defaultConfig = {
     // Root path of server
     root: path.normalize(__dirname + '/../../..'),
 
-  // Server port
-  port: process.env.PORT || 9000,
+    // Server port
+    port: process.env.PORT || 9000,
 
-  // Should we populate the DB with sample data?
-  seedDB: false,
+    seedDB: false,
 
-  // Secret for session, you will want to change this and make it an environment variable
-  secrets: {
-    session: 'mrcabdriver-secret'
-  },
+    secrets: {
+        session: 'mrcabdriver-secret'
+    },
 
-  // List of user roles
-  userRoles: ['guest', 'user', 'admin'],
+    userRoles: ['guest', 'user', 'admin'],
 
-  // MongoDB connection options
-  mongo: {
-    options: {
-      db: {
-        safe: true
-      }
-    }
+    mongo: {
+        options: {
+            db: {
+                safe: true
+            }
+        }
   }
-
 };
 
-// Export the config object based on the NODE_ENV
-// ==============================================
 module.exports = _.merge(
-  all,
-  require('./' + process.env.NODE_ENV + '.js') || {});
+    defaultConfig,
+    require('./' + process.env.NODE_ENV + '.js') || {});
--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -1,16 +1,6 @@
 'use strict';
 
 module.exports = {
-  // Server IP todo: investigate
-  ip:       process.env.OPENSHIFT_NODEJS_IP ||
-            process.env.IP ||
-            undefined,
-
-  // Server port todo: investigate
-  port:     process.env.OPENSHIFT_NODEJS_PORT ||
-            process.env.PORT ||
-            8080,
-
     mongo: {
         uri: process.env.MONGOHQ_URL
     },
